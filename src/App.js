194d16c09582f56dbdb49a10dd5c6c1497449f02
donaldteghen <donaldteghen@gmail.com>
2021-03-14 11:45:48 +0300

    setup the admin folder for handling admin panel for the project. For the adminpart of the project, i used graphql support by react-apollo package. Also added authentication middleware to the server. Modified the app.js file to project access to admin and added reactcontext state object to manage authentication state of the user. Finally, for preparig a production ready setup, i changed the post, host and ulr in server and setup a productionjson data file to load data into the app to test product.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense} from "react";
 import { SportsStoreDataStore } from "./data/DataStore";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { ShopConnector } from "./shop/ShopConnector";
+//import { Admin } from "./admin/Admin";
+import { AuthProviderImpl } from "./auth/AuthProviderImpl";
+
+const Admin = lazy(() => import("./admin/Admin"));
 
 export default class App extends Component {
   render() {
   return <Provider store={ SportsStoreDataStore }>
-        <Router>
-        <Switch>
-        <Route path="/shop" component={ ShopConnector } />
-        <Redirect to="/shop" />
-        </Switch>
-        </Router>
+        <AuthProviderImpl>
+          <Router>
+            <Switch>
+              <Route path="/shop" component={ ShopConnector } />
+              <Route path="/admin"render={
+                routeProps =>
+                <Suspense fallback={ <h3>Loading...</h3> }>
+                <Admin { ...routeProps } />
+                </Suspense>
+              } />
+              <Redirect to="/shop" />
+            </Switch>
+          </Router>
+        </AuthProviderImpl>
         </Provider>
   }
 }
